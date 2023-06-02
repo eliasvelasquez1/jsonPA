4584dfcf734027fa7008e6e8479734da43ace879
eliasvelasquez1 <elias.velasquez03@inacapmail.cl>
2023-06-02 01:50:56 +0000

    Instrucciones en formato json

---
+++ api.py
@@ -0,0 +1,57 @@
+{ 
+  "semana": "3",
+  "Sensores": [
+    {
+      "equipo": "Host1",
+      "sector": "Pueblo Nuevo",
+     "dias": {
+        "Lunes": 18,
+        "Martes": 19,
+        "Miercoles": 17,
+        "Jueves": 21,
+        "Viernes": 22,
+        "Sabado": 24,
+        "Domingo": 22
+       }
+    },
+    {
+      "equipo": "Host2",
+      "sector": "Amanecer",
+     "dias": {
+        "Lunes": 20,
+        "Martes": 21,
+        "Miercoles": 19,
+        "Jueves": 22,
+        "Viernes": 22,
+        "Sabado": 23,
+        "Domingo": 22
+       }
+    },
+    {
+      "equipo": "Host3",
+      "sector": "Fundo el Carmen",
+     "dias": {
+        "Lunes": 20,
+        "Martes": 20,
+        "Miercoles": 20,
+        "Jueves": 21,
+        "Viernes": 23,
+        "Sabado": 23,
+        "Domingo": 23
+       }
+    },
+	{
+      "equipo": "Host4",
+      "sector": "Estadio",
+     "dias": {
+        "Lunes": 21,
+        "Martes": 21,
+        "Miercoles": 18,
+        "Jueves": 23,
+        "Viernes": 23,
+        "Sabado": 24,
+        "Domingo": 22
+       }
+    }
+  ]
+}
